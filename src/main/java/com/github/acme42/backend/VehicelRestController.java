705845ee699ba40802cc9651a98e27277e5e5848
czoller <czoller@users.noreply.github.com>
2020-12-30 16:36:21 +0100

    @PostMapping("/add") zu VehicelRestController hinzugefügt

--- src/main/java/com/github/acme42/backend/VehicelRestController.java
+++ src/main/java/com/github/acme42/backend/VehicelRestController.java
@@ -1,9 +1,12 @@
 package com.github.acme42.backend;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class VehicelRestController {
 
 	private static final Map<String, Set<String>> userVehicles = Map.of(
-		"Sam Jones", Set.of("Jupiter", "Merkur", "Juno"),
-		"Penny Morris", Set.of("Venus", "Neptun"),
-		"Elvis Cridlington", Set.of("Jupiter", "Venus"),
-		"Norris Steele", Set.of(),
-		"Tom Thomas", Set.of("Wallaby 1"),
-		"Norman Price", Set.of("Skateboard")
+		"Sam Jones", new HashSet<>(List.of("Jupiter", "Merkur", "Juno")),
+		"Penny Morris", new HashSet<>(List.of("Venus", "Neptun")),
+		"Elvis Cridlington", new HashSet<>(List.of("Jupiter", "Venus")),
+		"Norris Steele", new HashSet<>(List.of()),
+		"Tom Thomas", new HashSet<>(List.of("Wallaby 1")),
+		"Norman Price", new HashSet<>(List.of("Skateboard"))
 	);
 	
 	@GetMapping("/list")
@@ -25,5 +28,13 @@ public class VehicelRestController {
 			return userVehicles.get(user);
 		}
 		return Set.of();
+	}
+	
+	@PostMapping("/add")
+	public void addVehicleForUser(@RequestParam String vehicle, @RequestParam String user) {
+		if (!userVehicles.containsKey(user)) {
+			userVehicles.put(user, new HashSet<String>());
+		}
+		userVehicles.get(user).add(vehicle);
 	}
 }
